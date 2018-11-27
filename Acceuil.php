e49c74a108409a55fe7b1043463d306c7203c01f
Clemdubois <45402873+Clemdubois@users.noreply.github.com>
2018-11-27 20:04:47 +0100

    Add files via upload

---
+++ Acceuil.php
@@ -0,0 +1,29 @@
+.<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Projet</title>
+  </head>
+  <style>
+  body,h1,h2,h3,h4,h5,h6 {
+    font-family: "Raleway", sans-serif
+  }
+  body, html {
+      height: 100%;
+      line-height: 1.8;
+  }
+  .logo-id{
+    padding:8px 16px;
+    float:left;
+    width:auto;
+    border:none;
+    display:block;
+    outline:0
+  }
+
+
+  </style>
+  <body>
+    <?php include("menu.php");  ?>
+  </body>
+</html>
